975ebd1bf75806b4bba58c0e61fb3d401e0ba003
xupengfei <xupengfei08@163.com>
2016-10-27 11:46:04 +0800

    整理文档

--- flyingmemory/src/org/flyingmemory/dao/iface/TblMenuInfoDAO.java
+++
@@ -1,9 +0,0 @@
-package org.flyingmemory.dao.iface;
-
-import java.util.List;
-
-public interface TblMenuInfoDAO {
-
-	@SuppressWarnings("rawtypes")
-	public List findByHQLQuery(final String hql);
-}
